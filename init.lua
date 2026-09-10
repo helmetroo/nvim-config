cffded8f8bb37f9d8979286acfe3cd4b9631382e
Samuel Sweet <samueljsweet@gmail.com>
2026-09-10 11:40:58 -0600

    Use tsc instead of ts_ls

--- init.lua
+++ init.lua
@@ -755,7 +755,8 @@ do
     --    https://github.com/pmizio/typescript-tools.nvim
     --
     -- But for many setups, the LSP (`ts_ls`) will work just fine
-    ts_ls = {},
+    --ts_ls = {},
+    tsc = {},
 
     stylua = {}, -- Used to format Lua code
 
